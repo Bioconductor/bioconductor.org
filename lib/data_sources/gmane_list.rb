19d900df024418260bd9aecc279be45eec06a124
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-12-22 18:17:32 +0000

    First pass at gmane mailing list data source
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@43695 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ lib/data_sources/gmane_list.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+require 'rubygems'
+require 'httparty'
+require 'yaml'
+
+class GmaneList < Nanoc3::DataSource
+  identifier :gmane_list
+  def fetch
+    # FIXME: make this URL part of site config
+    bioc_list_url = ('http://rss.gmane.org/topics/excerpts/' +
+                     'gmane.science.biology.informatics.conductor')
+    data = HTTParty.get(bioc_list_url)
+    data["rdf:RDF"]["item"].map do |item|
+      attributes = {
+        :title => item["title"],
+        :date => item["dc:date"],
+        :link => item["link"],
+        :author => item["dc:creator"]
+      }
+      content = item["description"]
+      mtime = nil
+      identifier = "/#{File.basename(item["link"])}/"
+      Nanoc3::Item.new(content, attributes, identifier, mtime)
+    end
+  end
+  
+  def items
+    @items ||= fetch()
+  end
+end
+
