bfc454895f7dba1cc086a1c06504b0f877d4df19
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-02-04 01:07:56 +0000

    Add generated sitemap and start of redirects file
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@44480 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ scripts/compile_on_change.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+fsevent_dir = "~/src/ruby-fsevent"
+$LOAD_PATH.unshift(fsevent_dir + '/lib')
+$LOAD_PATH.unshift(fsevent_dir + '/ext')
+
+require 'fsevent'
+
+class PrintChange < FSEvent
+  def on_change(directories)
+    puts "Detected change in: #{directories.inspect}"
+    system "nanoc3 compile"
+  end
+
+  def start
+    puts "watching #{registered_directories.join(", ")} for changes"
+    super
+  end
+end
+
+printer = PrintChange.new
+printer.latency = 0.2
+printer.watch_directories %W(#{Dir.pwd}/content #{Dir.pwd}/layouts)
+printer.start
