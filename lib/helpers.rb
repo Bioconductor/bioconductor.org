2bdcdef5d861f78e1786a34708f974b727604112
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-02-09 01:42:45 +0000

    Use file extension to determine default filter
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@44569 bc3139a8-67e5-0310-9ffc-ced21a209358

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -49,3 +49,12 @@ def pluralize(count, what)
     what
   end
 end
+
+def add_missing_info 
+  items.each do |item| 
+    if item[:file] 
+      # nanoc3 >= 3.1 will have this feature, add for older versions 
+      item[:extension] ||= item[:file].path.match(/\..*$/)[0]
+    end 
+  end 
+end 
