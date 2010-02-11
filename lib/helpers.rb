97c238fe4eb6c427556c80d25c21883a54d92e0d
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-02-11 23:43:14 +0000

    Factor out sitehead and doctype from default layout
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@44666 bc3139a8-67e5-0310-9ffc-ced21a209358

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -68,3 +68,7 @@ def annual_reports
     }
   end
 end
+
+def doctype
+  %[<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">]
+end
