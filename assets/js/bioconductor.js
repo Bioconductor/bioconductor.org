fe5fbc1385107694c968155cf4a15ccf8c7a87df
p.aboyoun <p.aboyoun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-06-25 18:24:58 +0000

    Remake homepage in new style.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@47628 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ assets/js/bioconductor.js
@@ -0,0 +1,66 @@
+// bioconductor.js
+
+// general-use function to add handlers. use like this:
+//    if(document.getElementById('ehs.form')){
+//      addEvent(document.getElementById('ehs.form'), 'click', handleRadioClick);
+//    }
+// JS Bible 6th ed.
+function addEvent(elem,evtType,func){
+  if(elem.addEventListener){ elem.addEventListener(evtType,func,false); }
+  else if(elem.attachEvent){ elem.attachEvent("on"+evtType, func); }
+  else { elem["on"+evtType] = func; }
+}
+
+
+// parse the page and pick out div's that have a certain class
+// and change those into shaded boxes by adding HTML. this inserts
+// table code, but that should be transparent to all users.
+function renderShadedBoxes(){
+
+  // prepare the HTML to insert into the divs of target class
+  var insert1 = '<table cellspacing="0" cellpadding="0" class="sb"><tr><td class="sb1"></td><td class="sb2"></td><td class="sb3"></td></tr><tr><td class="sb4">&nbsp;</td><td class="sb5">';
+  var insert2 = '</td><td class="sb6">&nbsp;</td></tr><tr><td class="sb7"></td><td class="sb8"></td><td class="sb9"></td></tr></table>';
+
+  // obtain all the div's of the target class. note that pre-ie7 doesn't return .getAttribute('class') but does return .getAttribute('className') so we check for that specially
+  var oDivs = document.getElementsByTagName('div');
+  var className = '';
+  for(var i=0;i<oDivs.length;i++){
+    className = oDivs.item(i).getAttribute('class')||oDivs.item(i).getAttribute('className'); //alert(className);
+    if(className&&className.indexOf('shaded_box')>-1){  //alert(i);        
+      oDivs.item(i).innerHTML = insert1 + oDivs.item(i).innerHTML + insert2;    
+      oDivs.item(i).className=''; // this removes the shaded_box class from the original div so the styling i just made takes over
+    }
+  }
+  
+}
+
+// check each page load to see if there is any shaded_box class
+addEvent(window,'load',renderShadedBoxes);
+
+
+
+// Masthead site navigation. we have five or more site navigation elements
+// appearing at page top, and depending upon the current page url, we want
+// the corresponding element to be olive and color unchanged at hover. we do this by pattern matching
+// on the page url (client side), and turning the corresponding element olive.
+// the position of each of the patterns corresponds to the masthead nav element number,
+// e.g., the third element, /help/, which is index 3 (option base 1), matches masthead_nav_element_3 
+// we use one Array of matching patterns for each element in case one element needs to match more than one patten. 
+// examples are shown below, but adjust for your info architecture.
+var masthead_nav_elements = Array( Array('home.html'),Array('/install/','install.html'),Array('/help/'),Array('/developers/'),Array('/about/') );
+function checkNav(){
+  for(var i=0; i<masthead_nav_elements.length; i++){
+  for(var j=0; j<masthead_nav_elements[i].length; j++){
+    if( masthead_nav_elements[i]&&masthead_nav_elements[i][j] ){ // skips elements that are blank
+      if( window.location.href.indexOf(masthead_nav_elements[i][j])>-1 ){
+        // match at element i. make it olive
+        if( document.getElementById('masthead_nav_element_'+(i+1)) ){
+            document.getElementById('masthead_nav_element_'+(i+1)).className='masthead_nav_element masthead_nav_element_selected'; 
+            return; // matched, so no need to continue checking.
+        }
+      }
+    }
+  }}
+}
+addEvent(window,'load',checkNav);
+                                      
