bf21f5045b9f2b00610fbef613fe3e3d6f7982c2
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-09-07 21:11:59 +0000

    may be ready to go on production
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@49279 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/search_indexer.rb
+++ scripts/search_indexer.rb
@@ -3,14 +3,18 @@
 require 'pp'
 require 'fileutils'
 require 'yaml'
+require 'open3'
 
 include FileUtils
+include Open3
 
 class SearchIndexer
 
   def initialize()
     cache = {}
     cache_exists = false
+
+    curl_path = `which curl`.chomp
 
     pwd = FileUtils.pwd()
 
@@ -21,6 +25,9 @@ class SearchIndexer
       cache = YAML.load_file(cachefilename)
       cache_exists = true
     end
+
+
+    #puts "cache_exists= #{cache_exists}"; exit if true
 
     dirtoindex = "./output"
 
@@ -39,7 +46,6 @@ class SearchIndexer
 
     goodfiles = allfiles.grep(regex)
 
-
     url = "http://localhost:8983/solr/update"
 
     puts "#/bin/sh\n"
@@ -47,24 +53,41 @@ class SearchIndexer
     goodfiles.each do |file|
       cleanfile = file.gsub(/^\./,"")
       mtime = File.stat(file).mtime().to_i()
-      if ((!cache_exists) or (cache_exists && cache.has_key?(file) && mtime > cache[file]))
-        nice_name = file.gsub(/index\.html$/,"")
+      if ( \
+        (!cache_exists) \
+        or \
+        (cache_exists and cache.has_key?(file) and mtime > cache[file]) \
+        or \
+        (cache_exists and !cache.has_key?(file)) \
+      )
+        nice_name = cleanfile.gsub(/index\.html$/,"")
 
         puts %Q(echo "indexing #{nice_name}")
-        cmd = %Q(curl -s "#{url}/extract?literal.id=#{nice_name}&commit=false" -F "myfile=@#{dirtoindex}#{cleanfile}")
+        cmd = %Q(#{curl_path} -s "#{url}/extract?literal.id=#{nice_name}&commit=false" -F "myfile=@#{dirtoindex}#{cleanfile}")
         puts cmd
-        #  result = `#{cmd}`
-        #  puts result
+        #result = system(cmd)
+        #{}`#{cmd}`
+        #puts "#{result}\t#{nice_name}"
+        
+        #puts "#{nice_name}"
+        #Open3.popen3(cmd) do |stdin, stdout, stderr|
+        #  puts "stderr:"
+        #  puts stderr.readlines
+        #  puts "stdout:"
+        #  puts stdout.readlines
+        #  puts
+        #end
         
       end
       cache[file] = mtime
-
+      # todo - deal with files that have been removed since last run
 
     end
 
 
 
     cmd = "curl -s #{url} --data-binary '<commit/>' -H 'Content-type:text/xml; charset=utf-8'"
+    #system(cmd)
     puts cmd
     #`#{cmd}`
 
