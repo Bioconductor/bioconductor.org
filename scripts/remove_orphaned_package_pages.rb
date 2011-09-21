50723b29b06789464ddace76fb82ea4b919f070e
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-21 19:32:44 +0000

    add script to prune old package pages
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@58298 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ scripts/remove_orphaned_package_pages.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+if ARGV.size != 1
+  puts "supply web root"
+  exit
+end
+
+webroot = ARGV.first
+
+require 'rubygems'
+require 'json'
+require 'pp'
+require 'fileutils'
+
+json_root = "#{webroot}/packages/json"
+
+file_list = `find #{json_root}|grep "packages\\.json"`
+files = file_list.split("\n")
+
+deletion_list = []
+
+for file in files
+  
+  path = file.gsub("#{json_root}/", "")
+  segs = path.split("/")
+  version = segs.shift
+  segs.pop
+  repo = segs.join("/")
+  
+  #puts "\nversion = #{version}, repo = #{repo}\n"
+  
+  f = File.open(file)
+  json = f.readlines.join("\n")
+  obj = JSON.parse(json)
+  packages = obj.keys
+  next if packages.empty? # for safety
+  
+  dir = "#{webroot}/packages/#{version}/#{repo}/html"
+  existing_files = `ls -1 #{dir}`.split("\n")
+  
+  existing_packages = []
+  for pkg in existing_files
+    existing_packages.push pkg.sub(/\.html$/, "")
+  end
+  
+  files_to_delete = existing_packages - packages
+  
+  next if files_to_delete.empty?
+
+  for item in files_to_delete
+    deletion_list.push "#{dir}/#{item}.html"
+  end
+  
+end
+
+for item in deletion_list
+  puts "deleting #{item}"
+  FileUtils.rm item
+end
