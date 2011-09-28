99548f980baa5e19a3c48257c716f237fcdcc924
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-09-28 18:29:59 +0000

    make sure fields are filled out (in case javascript validation is disabled
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@58527 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/index.php
+++ captcha/index.php
@@ -104,20 +104,23 @@ Code:<br/>
 
   if($valid == true) {
     //echo "<center>Thanks, you entered the correct code.<br/>Click <a href=\"{$_SERVER['PHP_SELF']}\">here</a> to go back.</center>";
-    echo "<center>Thank you. Your email will be posted.</center>\n";
-    echo "<center><a href='http://bioconductor.org'>Return to Bioconductor Site</a></center>\n";
-    $listemail = "bioconductor@r-project.org";
-    $guestemail = "guest@bioconductor.org";
-    $sender = $_POST['name'] . " [guest] <" . $guestemail . ">";
-    $mailheaders = "From: " . $sender . "\n";
-    $to = $listemail . ", " . $_POST['email'];
-    $subject = $_POST['subject'];
-    $body = $_POST['body'] .
-     "\n\n -- output of sessionInfo(): \n\n" .
-     $_POST['sessioninfo'] .
-     "\n\n--\nSent via the guest posting facility at bioconductor.org.";
-    $result = mail($to, $subject, $body, $mailheaders);
-    
+    if ($_POST['subject'] == "") {
+        echo "<center>Please fill in all form items.  <a href=\"javascript:history.go(-1)\">Go back</a> to try again.</center>";
+    } else {
+        echo "<center>Thank you. Your email will be posted.</center>\n";
+        echo "<center><a href='http://bioconductor.org'>Return to Bioconductor Site</a></center>\n";
+        $listemail =  "dtenenba@fhcrc.org"; //"bioconductor@r-project.org";
+        $guestemail = "guest@bioconductor.org";
+        $sender = $_POST['name'] . " [guest] <" . $guestemail . ">";
+        $mailheaders = "From: " . $sender . "\n";
+        $to = $listemail . ", " . $_POST['email'];
+        $subject = $_POST['subject'];
+        $body = $_POST['body'] .
+         "\n\n -- output of sessionInfo(): \n\n" .
+         $_POST['sessioninfo'] .
+         "\n\n--\nSent via the guest posting facility at bioconductor.org.";
+        $result = mail($to, $subject, $body, $mailheaders);
+    }
   } else {
     echo "<center>Sorry, the code you entered was invalid.  <a href=\"javascript:history.go(-1)\">Go back</a> to try again.</center>";
   }
