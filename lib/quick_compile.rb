a657bd31e3e2bd12289fce1dddbbf4264473bd86
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-14 23:19:30 +0000

    add a quick-compile mechanism that works with newer versions of nanoc
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@59254 bc3139a8-67e5-0310-9ffc-ced21a209358

--- lib/quick_compile.rb
+++ lib/quick_compile.rb
@@ -3,9 +3,15 @@
 if ENV.has_key?("QUICK_NANOC_COMPILE") && ENV["QUICK_NANOC_COMPILE"] == "true"
   puts "suppressing GMANE and biocViews compilation"
   base_command = nil 
-  ObjectSpace.each_object(Nanoc3::CLI::Base) do |obj| 
+  ObjectSpace.each_object() do |obj| 
+    next unless obj.respond_to? :site
     base_command = obj 
-    sources = base_command.site.config[:data_sources]#.collect{|i|i[:type] == "filesystem_unified"}
-    base_command.site.config[:data_sources] = [sources.first]
+    begin
+      sources = base_command.site.config[:data_sources]
+      base_command.site.config[:data_sources] = [sources.first]
+    rescue
+    end
   end
 end
+
+
