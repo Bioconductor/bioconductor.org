f03b21838a28cef669e71b0fa4b601ed8f204298
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-24 22:56:09 +0000

    diagnostic code...
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63091 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/start_instance.php
+++ captcha/start_instance.php
@@ -14,9 +14,11 @@ if (empty($_POST)) {
         $py = "/home/webadmin/python/bin/python";
         $script = "/extra/www/event_reg/mailform/start_instance.py";
         $cmd = $py . " " . $script . " " . $ami_id;
-        echo("cmd = " . $cmd);
+        echo("cmd = " . $cmd . "\n<br/>");
         $last_line = exec($cmd, $output, $result_code);
-        echo("last_line = " . $last_line);
+        echo("last_line = " . $last_line . "\n<br/>");
+        echo("output = " . $output . "\n<br/>");
+        echo("result_code = " . $result_code . "\n<br/>");
         //header("Location: http://bioconductor.org/help/cloud/started?dns=" . $last_line);
     } else {
         header("Location: http://bioconductor.org/help/cloud/badcaptcha");
