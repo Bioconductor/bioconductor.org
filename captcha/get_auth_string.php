212d6c39817f8caf786fc42a069221d1493cd36b
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-25 00:08:07 +0000

    syntax fix
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63114 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/get_auth_string.php
+++ captcha/get_auth_string.php
@@ -1,5 +1,5 @@
 <?php
-    $url = $GET_["url"];
+    $url = $_GET["url"];
     echo("url = " . $url . "<br/>\n");
     $last_line = exec("/usr/bin/curl " . $url,  $output, $result_code);
     echo("result code = " . $result_code . "<br/>\n");
