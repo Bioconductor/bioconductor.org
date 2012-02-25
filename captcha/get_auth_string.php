2bbdf80f1dd2d1c67494faefb7d9dd66dbf40544
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-25 00:08:50 +0000

    clean up diagnostics
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63115 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/get_auth_string.php
+++ captcha/get_auth_string.php
@@ -1,8 +1,6 @@
 <?php
     $url = $_GET["url"];
-    echo("url = " . $url . "<br/>\n");
     $last_line = exec("/usr/bin/curl " . $url,  $output, $result_code);
-    echo("result code = " . $result_code . "<br/>\n");
     $ret = 'processResults("' . $last_line . '")';
     echo $ret;
 ?>
