ef621cc497102af10f806b6feddc22466b28347e
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-02-25 00:06:11 +0000

    explicit path to curl
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63112 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/get_auth_string.php
+++ captcha/get_auth_string.php
@@ -1,6 +1,7 @@
 <?php
     $url = $GET_["url"];
-    $last_line = exec("curl " . $url);
+    $last_line = exec("/usr/bin/curl " . $url,  $output, $result_code);
+    echo("result code = " . $result_code . "<br/>\n");
     $ret = 'processResults("' . $last_line . '")';
     echo $ret;
 ?>
