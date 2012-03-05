4f880919609213289ddfcb7e32fe47ddea1caffc
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-03-05 20:34:22 +0000

    add script to expire tryitnow instances after timeout
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63496 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ captcha/expire_instances.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+
+import sys
+import re
+import time
+import calendar
+import ConfigParser
+from boto import ec2
+import subprocess
+import urllib2
+
+config_file = "%s/credentials.cfg" % sys.path[0]
+
+config = ConfigParser.RawConfigParser()
+config.read(config_file)
+
+access_key = config.get("credentials", "access_key")
+secret_key = config.get("credentials", "secret_key")
+
+
+
+pat = re.compile("[0-9]{3}Z$")
+
+ec2conn = ec2.connection.EC2Connection(access_key, secret_key)
+reservations = ec2conn.get_all_instances()
+instances = [i for r in reservations for i in r.instances]
+for i in instances:
+    d = i.__dict__
+    if d['tags']['Name'] == 'tryitnow':
+        # figure out how long it has been running
+        lts = d['launch_time'] #format: 2009-10-27T17:10:22.000Z
+        lts = lts.sub(pat, "UTC", lts)
+        lt = time.strptime(t, "%Y-%m-%dT%H:%M:%S.%Z")
+        now = time.gmtime()
+        nowsecs = calendar.timegm(now)
+        ltsecs = calendar.timegm(lt)
+        diff = nowsecs - ltsecs
+        if (diff > (60 * 60 * 2)): # 2 hour timeout
+            print "Terminating %s launched at %s." % (d['id'], d['launch_time'])
+            i.terminate()
