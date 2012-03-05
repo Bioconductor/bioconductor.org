f9fc184e703b4408781ebed5adecca75ac194a6d
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-03-05 23:06:43 +0000

    try Location header
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63515 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/start_instance.php
+++ captcha/start_instance.php
@@ -29,8 +29,8 @@ if (empty($_POST)) {
         
         //$refreshtag =  "BLAHmeta http-equiv='refresh' content='0;url=http://bioconductor.org/help/cloud/started?dns='" . $dns . "&key=" . $key . "'>\n"; 
         //echo($refreshtag);
-        //header("Location: http://bioconductor.org/help/cloud/started?dns=" . $dns ."&key=" . $key);
-        echo("tryitnowCallbackFunc('" . $dns . "', '" . $key . "');");
+        header("Location: http://bioconductor.org/help/cloud/started?dns=" . $dns ."&key=" . $key);
+        //echo("tryitnowCallbackFunc('" . $dns . "', '" . $key . "');");
     } else {
         header("Location: http://bioconductor.org/help/cloud/badcaptcha");
     }
