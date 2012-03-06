94402bf5cff2175c8e7fc413e582347820c3a1a3
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-03-06 20:05:21 +0000

    diagnostics
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@63549 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/start_instance.php
+++ captcha/start_instance.php
@@ -9,6 +9,8 @@ if (empty($_POST)) {
     $ami_id = $_POST['ami_id'];
 
     if($valid == true) {
+        echo("Hello to you!<br/>\n");
+        flush();
         $py = "/home/webadmin/python/bin/python";
         $script = "/extra/www/event_reg/mailform/start_instance.py";
         $cmd = $py . " " . $script . " " . $ami_id;
@@ -16,7 +18,7 @@ if (empty($_POST)) {
         $segs = explode(";", $output[0]);
         $dns = $segs[0];
         $key = $segs[1];
-        /*
+        ///*
         echo("size of output: " . count($output) . "<br>\n");
         echo("last_line = " . $last_line . "<br>\n");
         echo("output = " . $output . "<br>\n");
@@ -25,11 +27,11 @@ if (empty($_POST)) {
         foreach ($output as $item) {
             echo("array item: " . $item . "<br>\n");
         }
-        */
+        //*/
         
         //$refreshtag =  "BLAHmeta http-equiv='refresh' content='0;url=http://bioconductor.org/help/cloud/started?dns='" . $dns . "&key=" . $key . "'>\n"; 
         //echo($refreshtag);
-        header("Location: http://bioconductor.org/help/cloud/started?dns=" . $dns ."&key=" . $key);
+        //header("Location: http://bioconductor.org/help/cloud/started?dns=" . $dns ."&key=" . $key);
         //echo("tryitnowCallbackFunc('" . $dns . "', '" . $key . "');");
     } else {
         header("Location: http://bioconductor.org/help/cloud/badcaptcha");
