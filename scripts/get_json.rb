af521370210f24030f4619e083bd775a29e38905
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-04-05 01:03:35 +0000

    more robustness
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@64877 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/get_json.rb
+++ scripts/get_json.rb
@@ -34,9 +34,9 @@ class GetJson
   def get_dcfs(repo, version)
     url = URI.parse("http://bioconductor.org/packages/#{version}/#{repo}/VIEWS")
     req = Net::HTTP::Get.new(url.path)
-        res = Net::HTTP.start(url.host, url.port) {|http|
-          http.request(req)
-        }
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
     views = res.body
     view_dcfs = []
     view_lines = views.split("\n")
@@ -49,7 +49,11 @@ class GetJson
         end
       end
       if line.empty?
-        pdcf = Dcf.parse(dcf)
+        ## this DCF parser is slow. Here's a faster one:
+        ## http://gist.github.com/117293
+        ## but it does not like unescaped colons in values,
+        ## and also requires spaces after field names (and colons, presumably)
+        pdcf = Dcf.parse(dcf) 
         view_dcfs.push pdcf
         dcf = ""
       else
@@ -60,6 +64,7 @@ class GetJson
         end
       end
     end
+    puts 1
     return clean_dcfs(view_dcfs)
   end
   
@@ -86,7 +91,7 @@ class GetJson
   
   
   
-  def get_bioc_views(dcfs, repo)
+  def get_bioc_views(dcfs, repo, version)
     default_view = "Software" if repo == "bioc"
     default_view = "AnnotationData" if repo == "data/annotation"
     default_view = "ExperimentData" if repo == "data/experiment"
@@ -100,33 +105,43 @@ class GetJson
       g.add_edge(row.first, row.last)
       sort_order.push row.first
     end
+    node_attrs = {}
     dcfs.each_pair do |key, value|
       if value.has_key? "biocViews"
-        for view in value["biocViews"]
-          if g.has_vertex? (view)
-            g.add_edge(view, )
-          else
-            print "[WARNING] non-existent biocView #{view} in package #{key}."
-          end
+        if value["biocViews"].nil? or value["biocViews"].empty?
+          bioc_views = []
+        else
+          bioc_views = value["biocViews"] << default_view
         end
       else
-        print "[WARNING] Package #{key} has no biocViews."
-        # deal with default_view
+        bioc_views = []
+      end
+      bioc_views.uniq!
+      bioc_views.sort!
+      value['biocViews'] = bioc_views
+      for bioc_view in bioc_views
+        if g.has_vertex? bioc_view
+          node_attrs[bioc_view] = {} unless node_attrs.has_key? bioc_view
+          node_attrs[bioc_view]['packageList'] = [] unless node_attrs[bioc_view].has_key? 'packageList'
+          node_attrs[bioc_view]['packageList'].push key
+        else
+          puts "[Warning] #{repo} package #{key} has invalid view #{bioc_view}. (BioC #{version})"
+        end
       end
     end
+    #pp node_attrs
   end
   
   def initialize(repo, version, outdir)
     dcfs = get_dcfs(repo, version)
-    #pp dcfs
     json = JSON.pretty_generate(dcfs)
     pkgs_file = File.open("#{outdir}/packages.json", "w")
     pkgs_file.print(json)
     pkgs_file.close
-    bioc_views = get_bioc_views(dcfs, repo)
+    bioc_views = get_bioc_views(dcfs, repo, version)
   end
 end
 
 
-j = GetJson.new("bioc", "2.11", "/Users/dtenenba/tmp")
+j = GetJson.new("bioc", "2.11", "#{ENV['HOME']}/tmp")
 
