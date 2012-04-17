030c0bda91acf6874748069b3eda94585aeb7f58
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-04-17 17:42:00 +0000

    use a different email validator
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@65223 bc3139a8-67e5-0310-9ffc-ced21a209358

--- captcha/send_mail.php
+++ captcha/send_mail.php
@@ -1,4 +1,76 @@
 <?php
+/**
+Validate an email address.
+Provide email address (raw input)
+Returns true if the email address has the email 
+address format and the domain exists.
+*/
+function validEmail($email)
+{
+   $isValid = true;
+   $atIndex = strrpos($email, "@");
+   if (is_bool($atIndex) && !$atIndex)
+   {
+      $isValid = false;
+   }
+   else
+   {
+      $domain = substr($email, $atIndex+1);
+      $local = substr($email, 0, $atIndex);
+      $localLen = strlen($local);
+      $domainLen = strlen($domain);
+      if ($localLen < 1 || $localLen > 64)
+      {
+         // local part length exceeded
+         $isValid = false;
+      }
+      else if ($domainLen < 1 || $domainLen > 255)
+      {
+         // domain part length exceeded
+         $isValid = false;
+      }
+      else if ($local[0] == '.' || $local[$localLen-1] == '.')
+      {
+         // local part starts or ends with '.'
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $local))
+      {
+         // local part has two consecutive dots
+         $isValid = false;
+      }
+      else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain))
+      {
+         // character not valid in domain part
+         $isValid = false;
+      }
+      else if (preg_match('/\\.\\./', $domain))
+      {
+         // domain part has two consecutive dots
+         $isValid = false;
+      }
+      else if
+(!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/',
+                 str_replace("\\\\","",$local)))
+      {
+         // character not valid in local part unless 
+         // local part is quoted
+         if (!preg_match('/^"(\\\\"|[^"])+"$/',
+             str_replace("\\\\","",$local)))
+         {
+            $isValid = false;
+         }
+      }
+      if ($isValid && !(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A")))
+      {
+         // domain not found in DNS
+         $isValid = false;
+      }
+   }
+   return $isValid;
+}
+
+
 session_start();  // Start the session where the code will be stored.
 if (empty($_POST)) {
     # don't do anything
@@ -13,7 +85,8 @@ if (empty($_POST)) {
               $_POST['email'] == "" ||
               $_POST['body'] == "" || 
               $_POST['code'] == "" || 
-              $_POST['sessioninfo'] == "") {
+              $_POST['sessioninfo'] == "" ||
+              !validEmail($_POST['email'])) {
           header("Location: http://bioconductor.org/help/mailing-list/mailform/missing_items");
       } else {
           if ($_POST['subject'] == "testignore") {
