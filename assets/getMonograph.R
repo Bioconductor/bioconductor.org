54ff8bf639f52f3ac7ef088a1bc1c3e10e6633f5
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2012-08-23 17:33:06 +0000

    use biocLite(monograph_group()) for versions of R that support BiocInstaller
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@68738 bc3139a8-67e5-0310-9ffc-ced21a209358

--- assets/getMonograph.R
+++ assets/getMonograph.R
@@ -1,10 +1,21 @@
-source("http://bioconductor.org/getBioC.R")
 
-getMonograph <- function(pkgs, groupName="monograph", ...)
+thisRVer <- paste(R.Version()[c("major", "minor")], collapse=".")
+if (thisRVer > "2.14")
 {
-    if (missing(pkgs))
-        biocinstall(groupName=groupName, ...)
-    else
-        biocinstall(pkgs=pkgs, groupName=groupName, ...)
-}
+    getMonograph <- function(...) 
+    {
+        source("http://bioconductor.org/biocLite.R")
+        biocLite(monograph_group())
+    }
+} else {
+    source("http://bioconductor.org/getBioC.R")
 
+    getMonograph <- function(pkgs, groupName="monograph", ...)
+    {
+        if (missing(pkgs))
+            biocinstall(groupName=groupName, ...)
+        else
+            biocinstall(pkgs=pkgs, groupName=groupName, ...)
+    }
+
+}
