8fb3ac5e3cd954688b180053f91f2767f5af5ff7
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-07-27 14:56:07 +0000

    start on rss feeds
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@78903 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ scripts/make_build_rss_feeds.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+require 'find'
+require 'rss'
+require 'pp'
+require 'yaml'
+
+DCFDIR="tmp/build_dcfs"
+
+def get_status(path)
+    f = File.open(path)
+    lines = f.readlines
+    statusline = lines.find {|i| i =~ /^Status: /}
+    statusline = statusline.chomp.sub(/^Status: /, "")
+    statusline
+end
+
+
+def runit()
+    pkglist = {}
+
+    config = YAML.load_file("./config.yaml")
+
+    Dir.chdir DCFDIR do
+        Find.find "." do |path|
+            next unless path =~ /\.dcf$/
+            segs = path.split("/")
+            pkg = segs.last.split(".").first
+    #        puts path
+            status = get_status(path)
+    #        puts status unless status == "OK"
+            pkglist[pkg] = [] unless pkglist.has_key? pkg
+            segs = path.split("/")
+            node =  segs[3]
+            version = segs[1]
+            phase = segs[4]
+            pkglist[pkg].push({:version => version, :node => node, :phase => phase, :status => status})
+        end
+    end
+
+    #puts pkglist.keys
+    #pp pkglist
+    pkglist
+end
+
+#runit()
+if $0 == __FILE__
+    runit()
+end
