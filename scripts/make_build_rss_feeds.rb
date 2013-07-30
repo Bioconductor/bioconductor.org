1bc2277f2fd127ada3043178ccf3d2ae1667459b
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-07-30 21:46:05 +0000

    makes build feeds
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@79118 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/make_build_rss_feeds.rb
+++ scripts/make_build_rss_feeds.rb
@@ -4,8 +4,11 @@ require 'find'
 require 'rss'
 require 'pp'
 require 'yaml'
+require 'fileutils'
 
 DCFDIR="tmp/build_dcfs"
+OUTDIR="assets/rss/build"
+BASEURL="http://bioconductor.org/checkResults"
 
 def get_status(path)
     f = File.open(path)
@@ -15,6 +18,74 @@ def get_status(path)
     statusline
 end
 
+def make_problem_feed(pkglist, config, problems, outfile)
+    rss = RSS::Maker.make("atom") do |maker|
+        maker.channel.author = "Bioconductor Build System"
+        maker.channel.updated = Time.now.to_s
+        ## FIXME: add content here:
+        maker.channel.about = "http://bioconductor.org/rss/build"
+        if problems.include? "WARNINGS"
+            maker.channel.title = "Build Problems (including warnings)"
+        else
+            maker.channel.title = "Build Problems (excluding warnings)"
+        end
+
+
+        for key in pkglist.keys.sort  {|a,b| b.downcase <=> a.downcase}
+            bad = pkglist[key].find_all {|i| problems.include? i[:status] }
+            for b in bad
+                maker.items.new_item do |item|
+                    item.link = "#{BASEURL}/#{b[:version]}/bioc-LATEST/#{key}/#{b[:node]}-#{b[:phase]}.html"
+                    item.title = "#{b[:status]} in #{b[:version]} version of #{key} on node #{b[:node]}"
+                    item.updated = Time.now.to_s
+                end
+            end
+        end
+    end
+    FileUtils.mkdir_p OUTDIR
+    f = File.open("#{OUTDIR}/#{outfile}", "w")
+    f.puts rss
+    f.close
+end
+
+def make_individual_feed(pkglist, config)
+    rootdir = "#{OUTDIR}/packages"
+    FileUtils.mkdir_p rootdir
+    for key in pkglist.keys
+        filename = "#{rootdir}/#{key}.rss"
+        file_exists = File.exist?(filename) and File.file?(filename)
+        bad = pkglist[key].find_all {|i| i[:status] != "OK"}
+        rss = RSS::Maker.make("atom") do |maker|
+            maker.channel.author = "Bioconductor Build System"
+            maker.channel.title = "Build Problems for #{key}"
+            maker.channel.updated = Time.now.to_s
+            ## FIXME: add content here:
+            maker.channel.about = "http://bioconductor.org/rss/build"
+
+            if bad.empty? and not file_exists
+                maker.items.new_item do |item|
+                    item.link = "#{BASEURL}/release/bioc-LATEST/#{key}/#{config["master_release_builder"]}-buildsrc.html"
+                    item.updated = Time.now.to_s
+                    item.title = "No build problems for #{key}."
+                end
+            else
+                for b in bad
+                    maker.items.new_item do |item|
+                        item.link = "#{BASEURL}/#{b[:version]}/bioc-LATEST/#{key}/#{b[:node]}-#{b[:phase]}.html"
+                        item.title = "#{b[:status]} in #{b[:version]} version of #{key} on node #{b[:node]}"
+                        item.updated = Time.now.to_s
+                    end
+                end
+            end
+
+        end
+        if (not bad.empty?) or (not file_exists)
+            f = File.open(filename, "w")
+            f.puts rss
+            f.close
+        end
+    end
+end
 
 def runit()
     pkglist = {}
@@ -26,9 +97,7 @@ def runit()
             next unless path =~ /\.dcf$/
             segs = path.split("/")
             pkg = segs.last.split(".").first
-    #        puts path
             status = get_status(path)
-    #        puts status unless status == "OK"
             pkglist[pkg] = [] unless pkglist.has_key? pkg
             segs = path.split("/")
             node =  segs[3]
@@ -39,8 +108,10 @@ def runit()
         end
     end
 
-    #puts pkglist.keys
-    #pp pkglist
+    make_problem_feed(pkglist, config, ["ERROR", "WARNINGS", "TIMEOUT"],
+        "problems.rss")
+    make_problem_feed(pkglist, config, ["ERROR", "TIMEOUT"], "errors.rss")
+    make_individual_feed(pkglist, config)
     pkglist
 end
 
