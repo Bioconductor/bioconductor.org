d3f309992201de1c3b28145d69b461a24c11cf30
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-08-03 21:51:59 +0000

    break into chunks
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@79231 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/ping_hub.rb
+++ scripts/ping_hub.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'yaml'
 require './scripts/PubSubHubbub-master/lib/pubsubhubbub'
+require 'pp'
 
 unless File.exists?("tmp/rss_urls.txt")
     puts "url file does not exist!"
@@ -10,17 +11,31 @@ unless File.exists?("tmp/rss_urls.txt")
 end
 
 config = YAML.load_file("./config.yaml")
-hub_url = config["rss_hub_url"]
+$hub_url = config["rss_hub_url"]
 
 lines = File.readlines "tmp/rss_urls.txt"
 
 feeds = lines.map{|i| i.chomp}
 
-EventMachine.run {
-  pub = EventMachine::PubSubHubbub.new(hub_url).publish feeds
-  pub.callback {
-    puts "Successfully notified hub." 
+
+CHUNKSIZE = 100
+
+
+def handle_chunk(chunk)
+    EventMachine.run {
+      pub = EventMachine::PubSubHubbub.new($hub_url).publish chunk
+      pub.callback {
+      puts "Successfully notified hub." 
     EventMachine.stop
   }
   pub.errback  { puts "Uh oh, something broke: #{pub.response}" }
 }
+end
+
+
+feeds.each_slice(CHUNKSIZE) do |chunk|
+    handle_chunk(feeds)
+end
+
+
+
