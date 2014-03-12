403ef3d0ccedd95c8e3e879f2d51285b5ef7001f
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-03-12 18:23:08 +0000

    show all entries in datatable
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@87368 bc3139a8-67e5-0310-9ffc-ced21a209358

--- assets/js/bioc_views.js
+++ assets/js/bioc_views.js
@@ -40,7 +40,7 @@ var displayPackages = function(packageList, nodeName) {
     jQuery.fn.dataTableExt.oStdClasses.sStripeEven = "row_even";
     jQuery("#packages").html(html);
     jQuery("#biocViews_package_table").dataTable({
-        "iDisplayLength": 50
+        "iDisplayLength": 5000
     });
 
 }
