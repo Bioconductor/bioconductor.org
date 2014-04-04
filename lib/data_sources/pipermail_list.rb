47eaf46ae00b2909d9b4bbc452d01af53f1d9d23
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-04-04 02:28:49 +0000

    almost works
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@88404 bc3139a8-67e5-0310-9ffc-ced21a209358

--- lib/data_sources/pipermail_list.rb
+++ lib/data_sources/pipermail_list.rb
@@ -6,7 +6,8 @@ require 'time'
 require 'yaml'
 require 'fileutils'
 require 'nokogiri'
-require 'openuri'
+require 'open-uri'
+require 'date'
 
 class PipermailList < Nanoc3::DataSource
     identifier :pipermail_list
@@ -19,7 +20,8 @@ class PipermailList < Nanoc3::DataSource
         year_last_month = (Date.today<<1).strftime("%Y")
         url = "#{baseurl}/#{this_year}-#{this_month}/date.html"
         # TODO ensure url exists?
-        doc = Nokogiri::HTML(open(url))
+        # why is the certificate not valid from dan's home laptop?
+        doc = Nokogiri::HTML(open(url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
         uls = doc.css("ul")
         return [] if uls.empty?
         msgul = uls[1]
@@ -27,8 +29,26 @@ class PipermailList < Nanoc3::DataSource
         a = msgul.css("a")
         return [] if a.empty?
         num_wanted = 5
-        
-
+        index_of_first = nil
+        if a.length > num_wanted * 2
+            index_of_first = a.length - (num_wanted * 2) - 1
+        else
+            index_of_first = 0
+        end
+        ret = []
+        for i in index_of_first..a.length-1
+            next unless i % 2 == 0
+            x = a[i]
+            href = x.attr('href')
+            subject = x.text.strip.sub(/^\[BioC\] /, "")
+            msg_url = url.sub("date.html", href)
+            msg_doc = Nokogiri::HTML(open(msg_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}))
+            datestamp = msg_doc.css("i").first.text#.gsub(/[ ]{2,}/," ")
+            dt = DateTime.strptime(datestamp, "%a %b %e %H:%M:%S %Z %Y")
+            isodate = dt.iso8601
+            puts i
+            puts a[i]
+        end
     end
 
 
