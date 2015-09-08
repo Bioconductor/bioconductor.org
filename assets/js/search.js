abb8d1c1f44344ddc6963c7283d7ccb8ae9e3c0d
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-09-08 01:50:50 +0000

    remove obsolete comment
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@108258 bc3139a8-67e5-0310-9ffc-ced21a209358

--- assets/js/search.js
+++ assets/js/search.js
@@ -15,8 +15,6 @@ jQuery(function() {
 
 
 var getSearchUrl = function(query, start) {
-	// IMPORTANT! change url below (master0 -> master) when the 
-	// time is right.
 	var url = "//master.bioconductor.org/solr/default/select?indent=on&version=2.2&q=" + query + 
 	"&fq=&start=" + start +  "&rows=10&fl=id,score,title&qt=standard&wt=json&explainOther=&hl=on&hl.fl=&hl.fragsize=200";
 	return url;
