c9c70eb54f6ccf13a01767217faa61c3e8f75327
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-09-08 03:59:19 +0000

    more tweaks to image caption filter
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@108263 bc3139a8-67e5-0310-9ffc-ced21a209358

--- lib/image_captions.rb
+++ lib/image_captions.rb
@@ -4,12 +4,12 @@ class ImageFilter < Nanoc::Filter
   identifier :image_captions
   
   def run(content, params={})
-    content.gsub(/!\[([^\]]*)\]\(([^\]]*)\)/m) do
+    content.gsub(/!\[([^\]]+)\]\(([^\)]+)\)/m) do
       alt = $1
       src = $2
       fig = "![#{alt}](#{src})"
-      alt = Kramdown::Document.new(alt).to_html
-      fig << "\n<p class=\"caption\">#{alt}</p>" unless alt.empty?
+     
+      fig << "\n<div class=\"caption\">#{Kramdown::Document.new(alt).to_html}</div>" unless alt.empty?
       fig
 
     end
