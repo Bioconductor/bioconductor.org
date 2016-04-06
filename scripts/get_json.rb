73f9ec1da36712cbf64aa5e139abf5351c30a971
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-04-06 15:43:30 +0000

    force VIEWS content to UTF-8
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/bioconductor.org@115895 bc3139a8-67e5-0310-9ffc-ced21a209358

--- scripts/get_json.rb
+++ scripts/get_json.rb
@@ -46,9 +46,9 @@ module Dcf
     end
     ret
   end
-  
+
   def self.get_value_as_array(value, remove_version_specifiers=false)
-    return [] if value.empty? or value.nil? 
+    return [] if value.empty? or value.nil?
     value.gsub!(",,", "%%ESCAPED_COMMA%%")
     value.gsub!(", ", ",")
     segs = value.split(",")
@@ -59,7 +59,7 @@ module Dcf
     end
     segs
   end
-  
+
 end
 
 class RGL::DirectedAdjacencyGraph
@@ -70,7 +70,7 @@ class RGL::DirectedAdjacencyGraph
     end
     children
   end
-  
+
   def parents(child)
     parents = []
     for edge in self.edges
@@ -79,11 +79,11 @@ class RGL::DirectedAdjacencyGraph
     parents
   end
 end
-  
+
 
 class GetJson
-  
-  
+
+
   def get_dcfs(repo, version)
     url = URI.parse("http://master.bioconductor.org/packages/#{version}/#{repo}/VIEWS")
     req = Net::HTTP::Get.new(url.path)
@@ -91,10 +91,11 @@ class GetJson
       http.request(req)
     }
     views = res.body
+    views = views.force_encoding('UTF-8')
     view_dcfs = []
     view_lines = views.split("\n")
     dcf = ""
-    view_lines.each_with_index do |line, i| 
+    view_lines.each_with_index do |line, i|
       line = line.force_encoding("UTF-8") if line.respond_to? :force_encoding
       if i == (view_lines.length() -1)
         if !line.empty?
@@ -116,8 +117,8 @@ class GetJson
     end
     return clean_dcfs(view_dcfs)
   end
-  
-  
+
+
   def clean_dcfs(dcfs)
     ret = {}
     plural_fields = ["Depends", "Suggests", "Imports", "Enhances", "biocViews",
@@ -134,8 +135,8 @@ class GetJson
     end
     ret
   end
-  
-  
+
+
   def get_bioc_views(dcfs, repo, version)
     default_view = "Software" if repo == "bioc"
     default_view = "AnnotationData" if repo == "data/annotation"
@@ -155,7 +156,7 @@ class GetJson
           f.write resp
       end
       #`curl -s -u  readonly:readonly #{url} > #{dir}/biocViewsVocab.sqlite`
-      dbfile = "#{dir}/biocViewsVocab.sqlite"      
+      dbfile = "#{dir}/biocViewsVocab.sqlite"
       db = SQLite3::Database.new(dbfile)
       rows = db.execute("select * from biocViews")
     end
@@ -204,27 +205,27 @@ class GetJson
         end
       end
       nodes_to_delete.push node if delete_parent
-      
+
     end
-    
-    
-    # todo - better way to delete nodes-- build up a nodeList from the 
+
+
+    # todo - better way to delete nodes-- build up a nodeList from the
     # biocViews of packages, uniq it, then delete all nodes not in that list.
     for node_to_delete in nodes_to_delete.uniq
       # disabling deletion, because we want to include orphan nodes
       # let javascript handle filtering them out
   #####    g.remove_vertex node_to_delete
     end
-    
-    
-    
+
+
+
     for node in g.vertices
       pkgs = []
       g.depth_first_visit(node) do |n|
         pkgs += node_attrs[n]['packageList'] if node_attrs.has_key? n and \
           node_attrs[n].has_key? 'packageList'
       end
-      
+
       node_attrs[node] = {} unless node_attrs.has_key? node
       node_attrs[node]['packageList'] = \
         [] unless node_attrs[node].has_key? "packageList"
@@ -233,8 +234,8 @@ class GetJson
       end
       node_attrs[node]['childnum'] = pkgs.uniq.length
     end
-    
-    
+
+
     for node in g.vertices
       h = {}
       h["name"] = node
@@ -246,13 +247,13 @@ class GetJson
       else
         h['packageList'] = []
       end
-    
+
       ret[node] = h
     end
     ret
   end
-  
-  
+
+
   def initialize(version, outdir)
     @coder = HTMLEntities.new
     nanoc_dir = File.expand_path $0
@@ -270,23 +271,23 @@ class GetJson
       packages_data.push p
       biocviews_data.push b
     end
-    
+
     packages_data = get_reverse_dependencies(packages_data)
-    
+
     repos.each_with_index do |repo, i|
       write_packages_file(packages_data[i], version, repo, outdir)
     end
-    
+
     write_tree_file(repos, biocviews_data, version, outdir)
-    
+
   end
-  
+
   def write_tree_file(repos, data, version, outdir)
     args = [repos, data, "#{outdir}/#{version}/tree.json"]
     p = ParseBiocViews.new(args)
   end
-  
-  
+
+
   def write_packages_file(data, version, repo, outdir)
     fulloutdir = "#{outdir}/#{version}/#{repo}"
     json = JSON.pretty_generate(data)
@@ -304,8 +305,8 @@ class GetJson
     pkgs_file.print(json)
     pkgs_file.close
   end
-  
-  
+
+
   # the biocViews packages builds reverse dependencies, but
   # not across repositories, so we handle that here
   def get_reverse_dependencies(data)
@@ -323,8 +324,8 @@ class GetJson
         end
       end
     end
-    
-    
+
+
     fields = {"Suggests" => "suggestsMe", "Depends" => "dependsOnMe", "Imports" => "importsMe"}
     ret = []
     for datum in data
@@ -343,14 +344,14 @@ class GetJson
     end
     ret
   end
-  
-  
+
+
   def handle_repo(repo, version, outdir)
     fulloutdir = "#{outdir}/#{version}/#{repo}"
     FileUtils.mkdir_p fulloutdir unless Kernel.test(?d, fulloutdir)
     dcfs = get_dcfs(repo, version)
     bioc_views = get_bioc_views(dcfs, repo, version)
-    
+
     ## todo - remove this
     json = JSON.pretty_generate(bioc_views)
     views_file = File.open("#{fulloutdir}/biocViews.json", "w")
@@ -360,6 +361,3 @@ class GetJson
     [dcfs, bioc_views]
   end
 end
-
-
-
